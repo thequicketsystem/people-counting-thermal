34c8b48bfe86a006c8ff07bcc2e876bee8c2760b
Garrett Smith <ggasmithh@users.noreply.github.com>
2021-01-01 18:30:01 -0600

    First iteration

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ TEMP_MIN, TEMP_MAX = 6, 20
 DEPTH_SCALE_FACTOR = 255.0 / (TEMP_MAX - TEMP_MIN)
 DEPTH_SCALE_BETA_FACTOR = -TEMP_MIN * 255.0 / (TEMP_MAX - TEMP_MIN)
 
+# Source:
 # https://answers.opencv.org/question/210645/detection-of-people-from-above-with-thermal-camera/
 
 temp_data = np.empty([2, 2])
@@ -22,6 +23,7 @@ def counter() -> None:
         for y in range(IMG_HEIGHT):
             temp_data[x, y] = f[32 * (23 - x) + y]
 
+    # Image processing
     temp_data = temp_data * DEPTH_SCALE_FACTOR + DEPTH_SCALE_BETA_FACTOR
     temp_data[temp_data > 255] = 255
     temp_data[temp_data < 0] = 0
@@ -29,4 +31,57 @@ def counter() -> None:
 
     temp_data = cv2.resize(temp_data, dsize=(IMG_WIDTH * 10, IMG_HEIGHT * 10), interpolation=cv2.INTER_CUBIC)
     temp_data = cv2.normalize(temp_data, temp_data, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-    
+
+    colorized_temp_data = cv2.applyColorMap(temp_data, cv2.COLORMAP_JET)
+
+    temp_data = cv2.bilateralFilter(temp_data,9,150,150)
+    _, temp_data = cv2.threshold(temp_data, 210, 255, cv2.THRESH_BINARY)
+
+    kernel = np.ones((5,5), np.uint8)
+
+    temp_data = cv2.erode(temp_data, kernel, iterations = 1)
+    temp_data = cv2.dilate(temp_data, kernel, iterations = 1)
+
+    temp_data = cv2.morphologyEx(temp_data, cv2.MORPH_CLOSE, kernel)
+
+    temp_data = cv2.bitwise_not(temp_data)
+
+    # Blob detection
+    params = cv2.SimpleBlobDetector_Params()
+
+    # Change thresholds
+    params.minThreshold = 0;
+    params.maxThreshold = 255;
+
+    # Filter by Area.
+    params.filterByArea = True
+    params.minArea = 50
+    params.maxArea = 7000
+
+    # Filter by Circularity
+    params.filterByCircularity = True
+    params.minCircularity = 0.1
+
+    # Filter by Convexity
+    params.filterByConvexity = False
+    params.minConvexity = 0.01
+
+    # Filter by Inertia
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.1
+
+    detector = cv2.SimpleBlobDetector_create(params)
+
+    keypoints = detector.detect(temp_data)
+
+    # Draw circles around blobs and display count on screen
+    temp_data_with_keypoints = cv2.drawKeypoints(temp_data, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    cv2.putText(temp_data_with_keypoints, str(len(keypoints)), (20,80), cv2.FONT_HERSHEY_SIMPLEX, 3, (0,0,255),3)
+
+    cv2.imshow(temp_data_with_keypoints)
+
+    cv2.waitKey('q')
+
+if __name__ == '__main__':
+    while True:
+        counter()
