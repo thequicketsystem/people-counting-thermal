ad36ab581244f58f4471cccc56cb5e465fec65bd
Garrett Smith <ggasmithh@users.noreply.github.com>
2021-01-04 18:27:07 -0600

    Added rudimentary position detection

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import MLX90640 as mlx
 import numpy as np
 import cv2
-
-IMG_WIDTH, IMG_HEIGHT = 24, 32
-TEMP_MIN, TEMP_MAX = 6, 20
-
-DEPTH_SCALE_FACTOR = 255.0 / (TEMP_MAX - TEMP_MIN)
-DEPTH_SCALE_BETA_FACTOR = -TEMP_MIN * 255.0 / (TEMP_MAX - TEMP_MIN)
+from math import sqrt
 
 # Sources:
 # https://answers.opencv.org/question/210645/detection-of-people-from-above-with-thermal-camera/
 # https://www.learnopencv.com/blob-detection-using-opencv-python-c/
+# https://github.com/thequicketsystem/people-counting-visual/
+
+IMG_WIDTH, IMG_HEIGHT = 24, 32
+TEMP_MIN, TEMP_MAX = 6, 20
+
+SCALE_FACTOR = 10
+
+DEPTH_SCALE_FACTOR = 255.0 / (TEMP_MAX - TEMP_MIN)
+DEPTH_SCALE_BETA_FACTOR = -TEMP_MIN * 255.0 / (TEMP_MAX - TEMP_MIN)
 
 temp_data = np.empty([2, 2])
+
+# set up circle dimesions to simulate RFID reader detection area
+circleR  = (IMG_WIDTH * SCALE_FACTOR) // 3
+circleX, circleY = (IMG_WIDTH * SCALE_FACTOR) // 2, (IMG_HEIGHT * SCALE_FACTOR)  // 2
 
 def counter() -> None:
     mlx.setup(30)
@@ -30,7 +38,7 @@ def counter() -> None:
     temp_data[temp_data < 0] = 0
     temp_data = temp_data.astype('uint8')
 
-    temp_data = cv2.resize(temp_data, dsize=(IMG_WIDTH * 10, IMG_HEIGHT * 10), interpolation=cv2.INTER_CUBIC)
+    temp_data = cv2.resize(temp_data, dsize=(IMG_WIDTH * SCALE_FACTOR, IMG_HEIGHT * SCALE_FACTOR), interpolation=cv2.INTER_CUBIC)
     temp_data = cv2.normalize(temp_data, temp_data, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
 
     temp_data = cv2.bilateralFilter(temp_data, 9, 150, 150)
@@ -73,10 +81,27 @@ def counter() -> None:
 
     keypoints = detector.detect(temp_data)
 
+    # Determine which blobs are inside the circle and which are outside
+    insideReaderRange = 0
+    outsideReaderRange = 0
+
+    for kp in keypoints:
+        d = sqrt(pow(kp.pt[0] - circleX, 2) + pow(kp.pt[0] - circleY, 2))
+        if d < circleR:
+            insideReaderRange += 1
+        else:
+            outsideReaderRange += 1
+
+
     # Draw circles around blobs and display count on screen
     temp_data_with_keypoints = cv2.drawKeypoints(temp_data, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    cv2.putText(temp_data_with_keypoints, str(len(keypoints)), (20,80), cv2.FONT_HERSHEY_SIMPLEX, 3, (0,0,255),3)
 
+    # Draw count of blobs on-screen, inside circle, outside circle, and the circle itself
+    cv2.putText(temp_data_with_keypoints, f"count: {str(len(keypoints))}", (10, (IMG_HEIGHT * SCALE_FACTOR) - 80), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+	cv2.putText(temp_data_with_keypoints, f"in: {insideReaderRange}", (10, (IMG_HEIGHT * SCALE_FACTOR) - 100), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+	cv2.putText(temp_data_with_keypoints, f"out: {outsideReaderRange}", (10, (IMG_HEIGHT * SCALE_FACTOR) - 120), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+    cv2.circle(temp_data_with_keypoints, (circleX, circleY), circleR, (0, 255, 255), 2)
+    
     cv2.imshow(temp_data_with_keypoints)
 
     cv2.waitKey('q')
