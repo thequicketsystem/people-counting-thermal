b726c5ea3575c08c5254f36e42c8bae8e5b7604f
Garrett Smith <ggasmithh@users.noreply.github.com>
2021-03-10 21:18:38 -0600

    Filter by color, simplified output

--- main.py
+++ main.py
@@ -40,8 +40,6 @@ while True:
     temp_data = cv2.resize(temp_data, dsize=(IMG_WIDTH * SCALE_FACTOR, IMG_HEIGHT * SCALE_FACTOR))
     temp_data = cv2.normalize(temp_data, temp_data, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
 
-    colorized_temp_data = cv2.applyColorMap(temp_data, cv2.COLORMAP_JET)
-
     temp_data = cv2.bilateralFilter(temp_data, 9, 150, 150)
 
     kernel = np.ones((5,5), np.uint8)
@@ -62,48 +60,34 @@ while True:
 
     # Filter by Area.
     params.filterByArea = True
-    params.minArea = 600
-    params.maxArea = 4000
+    params.minArea = 400
+    params.maxArea = 3000
 
     # Filter by Circularity
     params.filterByCircularity = True
     params.minCircularity = 0.1
 
     # Filter by Convexity
-    params.filterByConvexity = False
-    params.minConvexity = 0.01
+    params.filterByConvexity = True
+    params.maxConvexity = 0.6
 
     # Filter by Inertia
     params.filterByInertia = True
-    params.minInertiaRatio = 0.1
+    params.minInertiaRatio = 0.01
+    
+    # Filter by Color
+    params.filterByColor = 1
+    params.blobColor = 0
 
     detector = cv2.SimpleBlobDetector_create(params)
 
     keypoints = detector.detect(temp_data)
-
-    # Determine which blobs are inside the circle and which are outside
-    insideReaderRange = 0
-    outsideReaderRange = 0
-
-    for kp in keypoints:
-        d = sqrt(pow(kp.pt[0] - circleX, 2) + pow(kp.pt[1] - circleY, 2))
-        if d < circleR:
-            insideReaderRange += 1
-        else:
-            outsideReaderRange += 1
-
-
+    
     # Draw circles around blobs and display count on screen
     temp_data_with_keypoints = cv2.drawKeypoints(temp_data, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     # Draw count of blobs inside circle and outside circle, as well as the circle itself
     cv2.putText(temp_data_with_keypoints, f"count: {str(len(keypoints))}", (10, (IMG_HEIGHT * SCALE_FACTOR) - 80), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-    cv2.putText(temp_data_with_keypoints, f"in: {insideReaderRange}", (10, (IMG_HEIGHT * SCALE_FACTOR) - 100), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-    cv2.putText(temp_data_with_keypoints, f"out: {outsideReaderRange}", (10, (IMG_HEIGHT * SCALE_FACTOR) - 120), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-    cv2.circle(temp_data_with_keypoints, (circleX, circleY), circleR, (0, 255, 255), 2)
-
-    # Side-by-side comparison of thermal data and detected blobs
-    composite = np.concatenate((colorized_temp_data, temp_data_with_keypoints), axis = 1)
 
     cv2.imshow("People Counting Subsystem (Thermal) Demo", composite)
 
