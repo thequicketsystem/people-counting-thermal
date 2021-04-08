6ab61ba173d28feb479aefdfc05d89ffbc92ad15
Garrett Smith <ggasmithh@users.noreply.github.com>
2021-04-07 20:29:12 -0500

    Ignores input if too cold

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ from math import sqrt
 IMG_WIDTH, IMG_HEIGHT = 32, 24
 TEMP_MIN, TEMP_MAX = 6, 20
 
+MIN_TEMP = 30
+
 SCALE_FACTOR = 10
 
 i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
@@ -45,34 +47,39 @@ params.minInertiaRatio = 0.01
 
 detector = cv2.SimpleBlobDetector_create(params)
 
+blank_frame = np.zeros(IMG_HEIGHT * SCALE_FACTOR, IMG_WIDTH * SCALE_FACTOR)
+
 def get_best_of_x(x: int) -> int:
     return max([get_frame_data() for i in range(x)])
 
 def get_frame_data() -> int:
-
     try:
         mlx.getFrame(f)
     except ValueError:
         pass
 
-    temp_data = np.array(f).reshape((IMG_HEIGHT, IMG_WIDTH))
+    count = 0
+    frame = blank_frame
 
-    temp_data = cv2.resize(temp_data, dsize=(IMG_WIDTH * SCALE_FACTOR, IMG_HEIGHT * SCALE_FACTOR))
-    temp_data = cv2.normalize(temp_data, temp_data, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-    temp_data = cv2.GaussianBlur(temp_data, (5, 5), 0)    
-    _, temp_data = cv2.threshold(temp_data, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    temp_data = cv2.bitwise_not(temp_data)
+    if max(f) > MIN_TEMP:
 
-    keypoints = detector.detect(temp_data)
+        temp_data = np.array(f).reshape((IMG_HEIGHT, IMG_WIDTH))
 
-    count = len(keypoints)
+        temp_data = cv2.resize(temp_data, dsize=(IMG_WIDTH * SCALE_FACTOR, IMG_HEIGHT * SCALE_FACTOR))
+        temp_data = cv2.normalize(temp_data, temp_data, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+        temp_data = cv2.GaussianBlur(temp_data, (5, 5), 0)    
+        _, temp_data = cv2.threshold(temp_data, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        temp_data = cv2.bitwise_not(temp_data)
 
-    # Draw circles around blobs and display count on screen
-    temp_data_with_keypoints = cv2.drawKeypoints(temp_data, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        keypoints = detector.detect(temp_data)
+
+        count = len(keypoints)
+
+        frame = cv2.drawKeypoints(temp_data, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     # Draw count of blobs inside circle and outside circle, as well as the circle itself
-    cv2.putText(temp_data_with_keypoints, f"count: {count}", (10, (IMG_HEIGHT * SCALE_FACTOR) - 80), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-    cv2.imshow("People Counting Subsystem (Thermal) Demo", temp_data_with_keypoints)
+    cv2.putText(frame, f"count: {count}", (10, (IMG_HEIGHT * SCALE_FACTOR) - 80), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+    cv2.imshow("People Counting Subsystem (Thermal) Demo", frame)
     cv2.waitKey(1)
 
     return(count)
